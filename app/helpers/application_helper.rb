71f490cc0ed89d0965bfbc8e4c270b2799020e6d
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-01 18:27:14 +0000

    Changed page title to format with/without a prefix
    - full_title helper that checks if provided "title" is empty
    - changed root/home pages to not prefix anything

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  # Returns an html page title, optionally adding a page-specific prefix
+  def full_title(page_title = "")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.empty? ?
+      base_title :
+      "#{page_title} | #{base_title}"
+  end
+  
 end
