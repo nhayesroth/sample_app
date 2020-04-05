2b31dba180f6fb151cb76af0e6426f198c9401fd
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-04 19:43:28 -0700

    Changed routes to remove /static_pages/

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   # StaticPages
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/home'
-  get 'static_pages/contact'
+  get	 '/about',   to: 'static_pages#about'
+	get  '/help',    to: 'static_pages#help'
+  get  '/home',    to: 'static_pages#home'
+  get  '/contact', to: 'static_pages#contact'
 end
