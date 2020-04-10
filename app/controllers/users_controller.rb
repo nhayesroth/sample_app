2068b48f06e143ca3e2890978751b23ef0b087ff
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-10 14:40:52 -0700

    Add gravatar to users/show w/css and also add a signup form for users/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # uncomment to cause rails server to open a console
+    # at this line whenever it executes
+    # debugger 
   end
-  
+
   def new
+    @user = User.new
   end
 end
