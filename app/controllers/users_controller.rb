ab22efdade44bf0458d088e37d612be23922c718
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-11 14:02:39 -0700

    Add logic to the signup page to show errors for invalid submissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if (@user.save())
+      show
+    else
+      render 'new'
+    end
+  end
+
+  private def user_params
+    return params
+      .require(:user)
+      .permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation)
+  end
+
 end
