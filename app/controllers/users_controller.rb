20bf0bb9ca82e640f90af4e3c50ea3510dc35e82
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-12 16:55:34 -0700

    Added success message when crating a new account.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if (@user.save())
+      flash[:success] = "Successfully signed up! Welcome!"
       redirect_to @user
     else
       render 'new'
