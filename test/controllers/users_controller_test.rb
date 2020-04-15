c3515eb66880c425b4daec181a460df17e19c28c
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-15 12:36:52 -0700

    Add users controller tests for login/edit pages

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "should get new user sign up" do
     get signup_path
     assert_response :success
     assert_select "title", "Sign up | #{ApplicationHelper::BASE_TITLE}"
+  end
+
+  test "should get login" do
+    get login_path
+    assert_response :success
+    assert_select "title", "Login | #{ApplicationHelper::BASE_TITLE}"
+  end
+
+  test "should get account settings" do
+    get edit_user_path(@user)
+    assert_response :success
+    assert_select "title", "Account Settings | #{ApplicationHelper::BASE_TITLE}"
   end
 
 end
