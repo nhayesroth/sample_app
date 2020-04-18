7dbda1f765112532615e265c599650cbe3766254
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-18 12:53:57 -0700

    Turn on logging (stdout info)
    Use ActiveSupport::Logger because Rails5 removed silence method (which causes css not to load)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure logger
+Rails.logger = ActiveSupport::Logger.new(STDOUT)
+Rails.logger.level = Logger::INFO
