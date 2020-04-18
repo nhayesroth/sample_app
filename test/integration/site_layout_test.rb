e725de7170ff30bf7b8ea4020783f9e6b104d5b2
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-18 13:33:40 -0700

    Split integration/site_layout_test into logged in/out cases

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # Tests the layout of the home page
-  test "layout links" do
+  test "layout links when logged out" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links when logged in" do
+    login
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", logout_path
   end
 end
