010dcd06a44d86d14cfb8864e91c759b471e2a56
nhayes-roth <nathan.hayesroth@gmail.com>
2020-04-20 13:31:06 -0700

    Added db/seed to create 100 users
    invoked via db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |n|
+  User.create!(
+    name: Faker::Name.name,
+    email: "fake-user#{n}@fakedomain.com",
+    password: "password",
+    password_confirmation: "password")
+end
